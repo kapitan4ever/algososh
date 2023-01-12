1d182165a500bf495794279c515dc3ff68b0dadb
kapitan4ever <stklmn@yandex.ru>
2023-01-12 23:48:22 +0300

    test string

--- src/components/string/utils.ts
+++ src/components/string/utils.ts
@@ -1,4 +1,5 @@
 import { ElementStates } from '../../types/element-states'
+import {swap} from '../../utils/utils'
 
 export const stateCircle = (index: number, step: number, arr: Array<string>) => {
 	if (index < step || index > arr.length - 1 - step) {
@@ -9,3 +10,12 @@ export const stateCircle = (index: number, step: number, arr: Array<string>) => 
 	}
 	return ElementStates.Default
 }
+
+export function reversedString (string: string) {
+	const arrayLetters = string.split('');
+	let endLetters = arrayLetters.length
+
+	for (let index = 0; index < Math.floor(endLetters / 2); index++) {
+		swap(arrayLetters, index, endLetters - 1)
+	}
+ }
