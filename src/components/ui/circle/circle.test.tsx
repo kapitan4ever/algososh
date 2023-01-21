27544c1542aabe461a4eaa2eb5a0d1280b7a2dc0
kapitan4ever <stklmn@yandex.ru>
2023-01-22 00:26:44 +0300

    test component button and circle

---
+++ src/components/ui/circle/circle.test.tsx
@@ -0,0 +1,62 @@
+import {render} from '@testing-library/react'
+import { ElementStates } from '../../../types/element-states'
+import { Circle } from './circle'
+
+describe('Circle', () => {
+	it('renders with correct text', () => {
+		const { getByTestId } = render(<Circle letter='circleExample' />)
+		const circle = getByTestId('circle')
+
+		expect(circle).toHaveTextContent('circleExample')
+		expect(circle).toMatchSnapshot()
+	})
+	it('renders without correct text', () => {
+		const { getByTestId } = render(<Circle />)
+		const circle = getByTestId('circle')
+
+		expect(circle).toBeInTheDocument()
+		expect(circle).toMatchSnapshot()
+	})
+	it('renders with correct text in head', () => {
+		const { getByTestId } = render(<Circle head='head' />)
+		const circle = getByTestId('circle')
+
+		expect(circle).toHaveTextContent('head')
+		expect(circle).toMatchSnapshot()
+	})
+	it('renders with correct text in tail', () => {
+		const { getByTestId } = render(<Circle tail='tail' />)
+		const circle = getByTestId('circle')
+
+		expect(circle).toHaveTextContent('tail')
+		expect(circle).toMatchSnapshot()
+	})
+	it('renders with correct text in react element head', () => {
+		const { getByTestId } = render(<Circle head={<a>head</a>} />)
+		const circle = getByTestId('circle')
+
+		expect(circle).toHaveTextContent('head')
+		expect(circle).toMatchSnapshot()
+	})
+	it('renders with correct text in react element tail', () => {
+		const { getByTestId } = render(<Circle tail={<a>tail</a>} />)
+		const circle = getByTestId('circle')
+
+		expect(circle).toHaveTextContent('tail')
+		expect(circle).toMatchSnapshot()
+	})
+	it('renders with correct index', () => {
+		const { getByTestId } = render(<Circle index={8} />)
+		const circle = getByTestId('circle')
+
+		expect(circle).toHaveTextContent('8')
+		expect(circle).toMatchSnapshot()
+	})
+	it('renders with correct isSmall', () => {
+		const { getByTestId } = render(<Circle isSmall />)
+		const circle = getByTestId('circle-main')
+
+		expect(circle).toHaveClass('small')
+		expect(circle).toMatchSnapshot()
+	})
+})
--- src/components/ui/circle/circle.tsx
+++ src/components/ui/circle/circle.tsx
@@ -23,41 +23,35 @@ export const Circle: React.FC<CircleProps> = ({
   isSmall,
 }) => {
   return (
-    <div className={`${styles.content} ${extraClass}`}>
-      <div
-        className={`text text_type_input text_color_input mb-4 ${
-          styles.absolute
-        } ${styles.head} ${
-          styles[typeof head === "string" ? "string" : "element"]
-        }`}
-      >
-        {head}
-      </div>
-      <div
-        className={`${styles.circle}  ${isSmall ? styles.small : ""} ${
-          styles[state]
-        }`}
-      >
-        <p
-          className={`text text_type_circle text_color_input ${styles.letter}`}
-        >
-          {letter}
-        </p>
-      </div>
-      <p
-        className={`text text_type_input text_color_input mt-4 ${styles.absolute} ${styles.index}`}
-      >
-        {index?.toString()}
-      </p>
-      <div
-        className={`text text_type_input text_color_input mt-4 ${
-          styles.absolute
-        } ${index?.toString() ? styles.tail60 : styles.tail30} ${
-          styles[typeof tail === "string" ? "string" : "element"]
-        }`}
-      >
-        {tail}
-      </div>
-    </div>
-  );
+		<div
+			className={`${styles.content} ${extraClass}`}
+			data-testid={isSmall ? 'circle-small' : 'circle'}
+		>
+			<div
+				className={`text text_type_input text_color_input mb-4 ${styles.absolute} ${styles.head} ${
+					styles[typeof head === 'string' ? 'string' : 'element']
+				}`}
+			>
+				{head}
+			</div>
+			<div
+				data-testid='circle-main'
+				className={`${styles.circle}  ${isSmall ? styles.small : ''} ${styles[state]}`}
+			>
+				<p className={`text text_type_circle text_color_input ${styles.letter}`}>{letter}</p>
+			</div>
+			<p
+				className={`text text_type_input text_color_input mt-4 ${styles.absolute} ${styles.index}`}
+			>
+				{index?.toString()}
+			</p>
+			<div
+				className={`text text_type_input text_color_input mt-4 ${styles.absolute} ${
+					index?.toString() ? styles.tail60 : styles.tail30
+				} ${styles[typeof tail === 'string' ? 'string' : 'element']}`}
+			>
+				{tail}
+			</div>
+		</div>
+	)
 };
