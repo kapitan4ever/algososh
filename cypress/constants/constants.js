d8f20aafb43123511ce8dd50232d2892ddd81d5d
kapitan4ever <stklmn@yandex.ru>
2023-01-29 19:40:39 +0300

    add 7 cypress test

--- cypress/constants/constants.js
+++ cypress/constants/constants.js
@@ -1,4 +1,5 @@
 export const mainCircle = '[class*="circle_circle"]'
+export const smallCircle = '[class*="circle_small"]'
 
 export const dataCyFormByIndex = '[data-cy="form__by_index"]'
 export const dataCyForm = '[data-cy="form"]'
@@ -7,10 +8,10 @@ export const dataCyInput = '[data-cy="input"]'
 export const dataCyIndex = '[data-cy="index"]'
 export const dataCySubmit = '[data-cy="submit"]'
 
-export const dataCyAdd = '[data-cy="add"]'
+export const dataCyAddTail = '[data-cy="add_tail"]'
 export const dataCyAddHead = '[data-cy="add_head"]'
 export const dataCyAddByIndex = '[data-cy="add_by_index"]'
-export const dataCyRemove = '[data-cy="remove"]'
+export const dataCyRemoveTail = '[data-cy="remove_tail"]'
 export const dataCyRemoveHead = '[data-cy="remove_head"]'
 export const dataCyRemoveByIndex = '[data-cy="remove_by_index"]'
 export const dataCyClear = '[data-cy="clear"]'
--- cypress/e2e/7-list.cy.ts
+++ cypress/e2e/7-list.cy.ts
@@ -1,1 +1,195 @@
-import { dataCyInput, dataCySubmit, mainCircle } from '../constants/constants'
+import {
+	dataCyAddByIndex,
+	dataCyAddHead,
+	dataCyAddTail,
+	dataCyInput,
+	mainCircle,
+	smallCircle,
+} from '../constants/constants'
+
+describe('Correct operation of Stack', () => {
+	beforeEach(() => {
+		cy.visit(`/list`)
+	})
+
+	it('If input empty button disabled', () => {
+		cy.get(dataCyInput).should('have.value', '')
+		cy.get(dataCyAddHead).should('be.disabled')
+		cy.get(dataCyAddTail).should('be.disabled')
+		cy.get(dataCyAddByIndex).should('be.disabled')
+	})
+
+	it('The default items are generated correctly', () => {
+		cy.get(mainCircle)
+			.should('have.length', 4)
+			.invoke('attr', 'class')
+			.then(classList => expect(classList).contains('circle_default'))
+	})
+
+	it('Add element in head', () => {
+		cy.get(dataCyInput).first().type('head')
+		cy.contains('Добавить в head').click()
+		cy.get(smallCircle)
+			.contains('head')
+			.parent()
+			.invoke('attr', 'class')
+			.then(classList => expect(classList).contains('circle_changing'))
+
+		cy.wait(500)
+
+		cy.get(mainCircle).contains('head').parent('div').siblings('p').contains('0')
+		cy.get(mainCircle)
+			.contains('head')
+			.parent()
+			.invoke('attr', 'class')
+			.then(classList => expect(classList).contains('circle_modified'))
+
+		cy.wait(500)
+
+		cy.get(mainCircle)
+			.contains('head')
+			.parent()
+			.invoke('attr', 'class')
+			.then(classList => expect(classList).contains('circle_default'))
+		cy.get(mainCircle).should('have.length', 5)
+	})
+
+	it('Add element in tail', () => {
+		cy.get(dataCyInput).first().type('tail')
+		cy.contains('Добавить в tail').click()
+		cy.get(smallCircle)
+			.contains('tail')
+			.parent()
+			.invoke('attr', 'class')
+			.then(classList => expect(classList).contains('circle_changing'))
+
+		cy.wait(500)
+
+		cy.get(mainCircle).contains('tail').parent('div').siblings('p').contains('4')
+		cy.get(mainCircle)
+			.contains('tail')
+			.parent()
+			.invoke('attr', 'class')
+			.then(classList => expect(classList).contains('circle_modified'))
+
+		cy.wait(500)
+
+		cy.get(mainCircle)
+			.contains('tail')
+			.parent()
+			.invoke('attr', 'class')
+			.then(classList => expect(classList).contains('circle_default'))
+		cy.get(mainCircle).should('have.length', 5)
+	})
+
+	it('Add element to index', () => {
+		cy.get('input').first().type('ind')
+		cy.get('input').last().type('1')
+		cy.contains('Добавить по индексу').click()
+		cy.get(smallCircle)
+			.contains('ind')
+			.parent()
+			.invoke('attr', 'class')
+			.then(classList => expect(classList).contains('circle_changing'))
+		cy.get(mainCircle)
+			.first()
+			.invoke('attr', 'class')
+			.then(classList => expect(classList).contains('circle_changing'))
+
+		cy.wait(500)
+
+		cy.get(mainCircle)
+			.eq(1)
+			.invoke('attr', 'class')
+			.then(classList => expect(classList).contains('circle_changing'))
+
+		cy.wait(500)
+
+		cy.get(mainCircle)
+			.contains('ind')
+			.parent()
+			.invoke('attr', 'class')
+			.then(classList => expect(classList).contains('circle_changing'))
+
+		cy.wait(500)
+
+		cy.get(mainCircle)
+			.contains('ind')
+			.parent()
+			.invoke('attr', 'class')
+			.then(classList => expect(classList).contains('circle_modified'))
+		cy.get(mainCircle).should('have.length', 5)
+	})
+
+	it('Remove element in head', () => {
+		cy.contains('Удалить из head').click()
+		cy.get(smallCircle)
+			.invoke('attr', 'class')
+			.then(classList => expect(classList).contains('circle_changing'))
+		cy.get(smallCircle)
+			.get('[class*=text_type_circle]')
+			.invoke('text')
+			.then(item => {
+				cy.get(mainCircle).first().should('not.have.text', item)
+			})
+
+		cy.wait(500)
+
+		cy.get(mainCircle).first().siblings('p').contains('0')
+		cy.get(mainCircle).should('have.length', 3)
+	})
+
+	it('Remove element in tail', () => {
+		cy.contains('Удалить из tail').click()
+		cy.get(smallCircle)
+			.invoke('attr', 'class')
+			.then(classList => expect(classList).contains('circle_changing'))
+		cy.get(smallCircle)
+			.get('[class*=text_type_circle]')
+			.invoke('text')
+			.then(item => {
+				cy.get(mainCircle).last().should('not.have.text', item)
+			})
+
+		cy.wait(500)
+
+		cy.get(mainCircle).last().siblings('p').contains('2')
+		cy.get(mainCircle).should('have.length', 3)
+	})
+
+	it('Correct deletion by index', () => {
+		cy.get('input').last().type('1')
+		cy.contains('Удалить по индексу').click()
+		cy.get(mainCircle)
+			.first()
+			.invoke('attr', 'class')
+			.then(classList => expect(classList).contains('circle_changing'))
+
+		cy.wait(500)
+
+		cy.get(mainCircle)
+			.eq(1)
+			.invoke('attr', 'class')
+			.then(classList => expect(classList).contains('circle_default'))
+
+		cy.wait(500)
+
+		cy.get(smallCircle)
+			.invoke('attr', 'class')
+			.then(classList => expect(classList).contains('circle_changing'))
+		cy.get(smallCircle)
+			.get('[class*=text_type_circle]')
+			.invoke('text')
+			.then(item => {
+				cy.get(mainCircle).last().should('not.have.text', item)
+			})
+		cy.get(mainCircle)
+			.eq(2)
+			.invoke('attr', 'class')
+			.then(classList => expect(classList).contains('circle_changing'))
+
+		cy.wait(500)
+
+		cy.get(mainCircle).should('have.length', 3)
+	})
+})
