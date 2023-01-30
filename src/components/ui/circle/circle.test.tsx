aa673202771e4ce1c6fbee825d72b7917fb3a634
kapitan4ever <stklmn@yandex.ru>
2023-01-30 22:02:57 +0300

    not work branch

--- src/components/ui/circle/circle.test.tsx
+++ src/components/ui/circle/circle.test.tsx
@@ -1,4 +1,4 @@
-import {render} from '@testing-library/react'
+import { render } from '@testing-library/react'
 import { ElementStates } from '../../../types/element-states'
 import { Circle } from './circle'
 
@@ -57,27 +57,6 @@ describe('Circle', () => {
 		const circle = getByTestId('circle-main')
 
 		expect(circle).toHaveClass('small')
-		expect(circle).toMatchSnapshot()
-	})
-	it('renders with correct state Default', () => {
-		const { getByText } = render(<Circle state={ElementStates.Default} letter='default'/>)
-		const circle = getByText('default')
-
-		expect(circle).toHaveTextContent('default')
-		expect(circle).toMatchSnapshot()
-	})
-	it('renders with correct state Modified', () => {
-		const { getByText } = render(<Circle state={ElementStates.Modified} letter='modified' />)
-		const circle = getByText('modified')
-
-		expect(circle).toHaveTextContent('modified')
-		expect(circle).toMatchSnapshot()
-	})
-	it('renders with correct state Changing', () => {
-		const { getByText } = render(<Circle state={ElementStates.Changing} letter='changing' />)
-		const circle = getByText('changing')
-
-		expect(circle).toHaveTextContent('changing')
 		expect(circle).toMatchSnapshot()
 	})
 })
--- src/components/ui/circle/circle.tsx
+++ src/components/ui/circle/circle.tsx
@@ -1,31 +1,29 @@
-import React from "react";
-import styles from "./circle.module.css";
-import { ElementStates } from "../../../types/element-states";
+import React from 'react'
+import styles from './circle.module.css'
+import { ElementStates } from '../../../types/element-states'
 
 interface CircleProps {
-  state?: ElementStates;
-  letter?: string;
-  head?: string | React.ReactElement | null;
-  index?: number;
-  tail?: string | React.ReactElement | null;
-  tailType?: "string" | "element";
-  extraClass?: string;
-  isSmall?: boolean;
+	state?: ElementStates
+	letter?: string
+	head?: string | React.ReactElement | null
+	index?: number
+	tail?: string | React.ReactElement | null
+	tailType?: 'string' | 'element'
+	extraClass?: string
+	isSmall?: boolean
 }
 
 export const Circle: React.FC<CircleProps> = ({
-  state = ElementStates.Default,
-  letter,
-  head,
-  index,
-  tail,
-  extraClass = "",
-  isSmall,
+	state = ElementStates.Default,
+	letter,
+	head,
+	index,
+	tail,
+	extraClass = '',
+	isSmall,
 }) => {
-  return (
-		<div
-			className={`${styles.content} ${extraClass}`}
-		>
+	return (
+		<div className={`${styles.content} ${extraClass}`}>
 			<div
 				className={`text text_type_input text_color_input mb-4 ${styles.absolute} ${styles.head} ${
 					styles[typeof head === 'string' ? 'string' : 'element']
@@ -34,13 +32,10 @@ export const Circle: React.FC<CircleProps> = ({
 				{head}
 			</div>
 			<div
+				data-testid='circle-main'
 				className={`${styles.circle}  ${isSmall ? styles.small : ''} ${styles[state]}`}
 			>
-				<p
-					className={`text text_type_circle text_color_input ${styles.letter}`}
-				>
-					{letter}
-				</p>
+				<p className={`text text_type_circle text_color_input ${styles.letter}`}>{letter}</p>
 			</div>
 			<p
 				className={`text text_type_input text_color_input mt-4 ${styles.absolute} ${styles.index}`}
@@ -56,4 +51,4 @@ export const Circle: React.FC<CircleProps> = ({
 			</div>
 		</div>
 	)
-};
+}
